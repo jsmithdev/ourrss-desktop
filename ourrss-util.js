dc6228f77650740f719e93640724999c10dd9b97
Jamie Smith <i@jamiesmiths.com>
2017-11-15 02:21:18 -0500

    modal dev; misc;

---
+++ ourrss-util.js
@@ -0,0 +1,21 @@
+module.exports = {
+    
+    message: function(mess) {
+
+        const Message = require('electron-notify');
+        const path = require('path');
+
+        Message.setConfig({
+            appIcon: path.join(`${__dirname}/img/icon.png`),
+            displayTime: 6000
+        });
+        Message.notify({
+            title: mess.head,
+            text: mess.body
+        });
+
+        return true;
+    }
+
+
+}
